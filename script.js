b29dc49e12499d81d3b599ff97b3033e5dfa33b5
Miko Brandini <MikoBrandini@users.noreply.github.com>
2016-11-07 16:31:18 -0500

    added content. beginning styling

--- script.js
+++ script.js
@@ -1,5 +1,57 @@
+//this is a global variable used several times:
+var questionCounter = 0;
+
+var whatToDoWithAnswers = function() {
+    $('.booleanChecker').click(function() {
+            var $target = $(event.target)
+            if ($target.hasClass('true')) {
+                $timerDiv.html(timeLeft += 8)
+                $('#progressInner').css('width', (questionCounter+1)*10+'%')
+                console.log('the time left are: ' + timeLeft)
+                $('#userInput').hide()
+                    //"replacement" moves the correct answer's button into the Upper Area
+                $('.replaceHere').replaceWith(($target))
+                $('#userInput').children('ul').remove()
+                $('.true').removeClass("true").addClass('learnMore ')
+                questionCounter += 1;
+                isPaused = true
+            }
+
+
+            else if ($target.hasClass('false')) {
+                //the esle statement means that a false answer was chosen.
+                $('.gameContainer').hide()
+                $('#userInput').children('ul').remove()
+                $('#ending').show()
+                isPaused = true
+                console.log("timer is paused")
+            }
+
+            else if ($target.hasClass('learnMore')) {
+                console.log("for some reason clicking on the correct answer is repeated twice. when it repeats twice, the second class i added to the button activates. I created that second class to be available only if the user themselves wants to click on it. this unplanned repeat if really annoyingg me. ")
+                window.open('https://tatoeba.org/eng/sentences/search?from=epo&to=eng&query=' + $('.learnMore').text())
+            }
+            // checking whether the game is over:
+            if (lvl1Questions.length == (questionCounter + 1)) {
+                console.log("this is the end")
+                $('#victory').show()
+
+            } else {
+                $('#inbetweenQuestions').show()
+                //here was the error saver
+            }
+
+        }) //end of booleanChecker
+
+}
+
+
+
+
+
+
 //code for Timer:
-timeLeft = 31
+timeLeft = 30
 $timerDiv = $('#timer')
     //paused time when game first loads.
 var isPaused = true;
@@ -85,7 +137,7 @@ var lvl1Questions = [
     {
         english: "Tomorrow will be beautiful!",
         question: "Morgaŭ" + '<span class="replaceHere"><button>blank</button></span>' + " bela!",
-     answers: [{
+        answers: [{
             text: "estas",
             boolean: false
         }, {
@@ -99,21 +151,22 @@ var lvl1Questions = [
             boolean: false
         }, {
             text: "estinteco",
-            boolean: true
+            boolean: false
         }, {
             text: "estos",
-            boolean: false
+            boolean: true
         }]
     },
+
     {
         english: "I will be there tomorrow!",
-        question: "Mi estos tie" + '<span class="replaceHere"><button>blank</button></span>' ,
-     answers: [{
+        question: "Mi estos tie" + '<span class="replaceHere"><button>blank</button></span>',
+        answers: [{
             text: "akvo",
             boolean: false
         }, {
             text: "morgaŭ",
-            boolean: false
+            boolean: true
         }, {
             text: "hundo",
             boolean: false
@@ -122,16 +175,17 @@ var lvl1Questions = [
             boolean: false
         }, {
             text: "ĉevala",
-            boolean: true
+            boolean: false
         }, {
             text: "bela",
             boolean: false
         }]
     },
+
     {
         english: "The girl is with the cat",
-        question: "La knabino estas kun la " + '<span class="replaceHere"><button>blank</button></span>' ,
-     answers: [{
+        question: "La knabino estas kun la " + '<span class="replaceHere"><button>blank</button></span>',
+        answers: [{
             text: "hundo",
             boolean: false
         }, {
@@ -142,193 +196,204 @@ var lvl1Questions = [
             boolean: false
         }, {
             text: "kato",
-            boolean: false
+            boolean: true
         }, {
             text: "viro",
-            boolean: true
+            boolean: false
         }, {
             text: "virino",
             boolean: false
         }]
+    }, {
+        english: "The woman is tall",
+        question: "La virino estas" + '<span class="replaceHere"><button>blank</button></span>',
+        answers: [{
+            text: "bela",
+            boolean: false
+        }, {
+            text: "rapida",
+            boolean: false
+        }, {
+            text: "viro",
+            boolean: false
+        }, {
+            text: "kato",
+            boolean: false
+        }, {
+            text: "alta",
+            boolean: true
+        }, {
+            text: "knabino",
+            boolean: false
+        }]
+    }, {
+        english: "The man fell",
+        question: "La viro" + '<span class="replaceHere"><button>blank</button></span>',
+        answers: [{
+            text: "estos",
+            boolean: false
+        }, {
+            text: "manĝos",
+            boolean: false
+        }, {
+            text: "dormas",
+            boolean: false
+        }, {
+            text: "falis",
+            boolean: true
+        }, {
+            text: "komprenas",
+            boolean: false
+        }, {
+            text: "komprenos",
+            boolean: false
+        }]
     },
 
-
-
+    {
+        english: "The young boy is playing with the cat.",
+        question: "La juna knabo" + '<span class="replaceHere"><button>blank</button></span> kun la kato',
+        answers: [{
+            text: "konis",
+            boolean: false
+        }, {
+            text: "kuiros",
+            boolean: false
+        }, {
+            text: "purigis",
+            boolean: false
+        }, {
+            text: "ludas",
+            boolean: true
+        }, {
+            text: "vidos",
+            boolean: false
+        }, {
+            text: "kuniklo",
+            boolean: false
+        }]
+    }, {
+        english: "I did not sleep yesterday",
+        question: "Mi ne" + '<span class="replaceHere"><button>blank</button></span> hieraŭ',
+        answers: [{
+            text: "dancos",
+            boolean: false
+        }, {
+            text: "estas",
+            boolean: false
+        }, {
+            text: "komprenas",
+            boolean: false
+        }, {
+            text: "hundo",
+            boolean: false
+        }, {
+            text: "dormis",
+            boolean: true
+        }, {
+            text: "preĝas",
+            boolean: false
+        }]
+    }
 ]
 
 
-// {english: "The girl is strong!",
-//  question: "La " + '<span class="replaceHere"><button>blank</button></span>' + " estas forta!",
-// answers:[
-// {text: "bebo",
-// boolean: false},
-// {text: "domo",
-// boolean: false},
-// {text: "knabino",
-// boolean: true},
-// {text: "filina",
-// boolean: false},
-// {text: "beba",
-// boolean: false},
-// {text: "ŝafo",
-// boolean: false}
-// ]
-// },
-
-// {english: "The boy is fast!",
-//  question: "La" + '<span class="replaceHere"><button>blank</button></span>'  + "estas rapida!",
-// answers:[
-// {text: "ĵaluza",
-// boolean: false},
-// {text: "rapido",
-// boolean: false},
-// {text: "bela",
-// boolean: false},
-// {text: "beba",
-// boolean: false},
-// {text: "knabo",
-// boolean: true},
-// {text: "kuniklo",
-// boolean: false}
-// ]
-// }
-// ]
-
 var makeNewQuestionAppear = function(questionNum) {
-        for (i = questionNum; i < lvl1Questions.length; i++) {
-            var english = lvl1Questions[i].english
-            $('#english').html(english)
 
-            var esperanto = lvl1Questions[i].question
-            $('#esperanto').html(esperanto)
+        i = questionNum
+        var english = lvl1Questions[i].english
+        $('#english').html(english)
+        var esperanto = lvl1Questions[i].question
+        $('#esperanto').html(esperanto)
+        $('#userInput').append('<ul id=ulAnswers>')
+        for (j = 0; j < lvl1Questions[i].answers.length; j++) {
+            //the following are the contents of the internal for loop
 
-            $('#userInput').append('<ul id=ulAnswers>')
-            for (j = 0; j < lvl1Questions[i].answers.length; j++) {
-                //the following are the contents of the internal for loop
+            var answer = lvl1Questions[i].answers[j].text
+            var correctness = lvl1Questions[i].answers[j].boolean
+            $('#ulAnswers').append('<li><button class="' + correctness + ' booleanChecker' + '">' + answer + '</button></li>')
+            if ($('#ulAnswers').children().size() == 6) {
+            whatToDoWithAnswers()
 
-                var answer = lvl1Questions[i].answers[j].text
-                var correctness = lvl1Questions[i].answers[j].boolean
-                $('#ulAnswers').append('<li><button class="' + correctness + ' booleanChecker' + '">' + answer + '</button></li>')
-                if ($('#ulAnswers').children().size() == 6) {
-                    return
-                }
-            } // end of internal for loop, answers display
-        } //end of for loop (iteration through question array)
-
+                return
+            }
+        }
     } //end of makeNewQuestionAppear
-
 makeNewQuestionAppear(questionCounter)
 
 
 
+$('#enterLevel1Button').on("click", function() {
+    $('#beginning').hide()
+    $('#lvl1').show()
+    isPaused = true
+    console.log("begin brotha")
 
-var addEventListeners = function() {
-        //I have a bunch of buttons and this was the easiest way for me to put them all in one place.
-        //Could be better organized, will do it later if I have time.
-        $('button').click(function(event) {
-                var $target = $(event.target)
-                if ($target.hasClass('questionToggler')) {
-                    questionCounter++;
-                    $('#inbetweenQuestions').hide()
-                    $('#userInput').show()
-                    makeNewQuestionAppear(questionCounter)
-                    isPaused = false
-                        //find me
-                        //setInterval was set here
-                    addEventListeners();
-                } else if ($target.hasClass('beginGame')) {
-                    $('#lvl1').hide(1000)
-                    $('.gameContainer').show()
-                    isPaused = false
-                    console.log("leaving instructions")
-                } else if ($target.hasClass('enterLevel1')) {
-                    $('#beginning').hide()
-                    $('#lvl1').show()
-                    isPaused = true
-                    console.log("begin brotha")
-                } else if ($target.hasClass('pronounciationButton')) {
-                    $('#pronounciationWrapper').show()
-                    isPaused = true
-                        //find me
-                        //setInterval was set here
-                } else if ($target.hasClass('learnMore')) {
-                    window.open('https://tatoeba.org/eng/sentences/search?from=epo&to=eng&query=' + $('.learnMore').text())
-                } else if ($target.hasClass('uniqueConsonantsToggler')) {
-                    $('#uniqueConsonantsTable').show()
-                    $('#diphthongsTable').hide()
-                    $('#samePronounciationTable').hide()
-                    $('#vowelsTable').hide()
-                    console.log("unique consonants")
-                } else if ($target.hasClass('vowelsToggler')) {
-                    $('#vowelsTable').css('display', 'table')
-                    $('#diphthongsTable').hide()
-                    $('#uniqueConsonantsTable').hide()
-                    $('#samePronounciationTable').hide()
-                    console.log("vowels should appear")
-                } else if ($target.hasClass('diphthongsToggler')) {
-                    $('#diphthongsTable').css('display', 'table')
-                    $('#vowelsTable').hide()
-                    $('#uniqueConsonantsTable').hide()
-                    $('#samePronounciationTable').hide()
-                    console.log("diphthongs should appear")
-                } else if ($target.hasClass('samePronounciationToggler')) {
-                    $('#samePronounciationTable').css('display', 'table')
-                    $('#diphthongsTable').hide()
-                    $('#vowelsTable').hide()
-                    $('#uniqueConsonantsTable').hide()
-                    console.log("samesies should appear")
-                }
-            }) //end of button listener
+})
+
+$('#beginGameButton').on("click", function() {
+    $('#lvl1').hide(1000)
+    $('.gameContainer').show()
+    isPaused = false
+    console.log("leaving instructions for level 1")
+})
 
 
 
-        //important gameContainer actions:
-        $('.booleanChecker').click(function(event) {
-                var $target = $(event.target)
-                if ($target.hasClass('true')) {
-                    $timerDiv.html(timeLeft += 8)
-                    $('#progressInner').css('width', '50%')
-                    console.log('the time left are: ' + timeLeft)
-                        //find me
-                        //clearInterval was here
-                    $('#userInput').hide()
-                    $('.replaceHere').replaceWith(($target).removeClass("true").addClass('learnMore '))
-                        //"replacement" moves the correct answer's button into the Upper Area
-                    $('#userInput').children('ul').remove()
-                    isPaused = true
-                } else if ($target.hasClass('false')) {
-                    //the esle statement means that a false answer was chosen.
-                    $('.gameContainer').hide()
-                    $('#userInput').children('ul').remove()
-                    $('#ending').show()
-                    isPaused = true
-                    console.log("timer is paused")
-                }
-                //checking whether the game is over:
-                if (lvl1Questions.length == (questionCounter + 1)) {
-                    console.log("this is the end")
-                    $('#victory').show()
 
-                } else {
-                    $('#inbetweenQuestions').show()
-
-                }
-
-            }) //end of booleanChecker
+$('.questionToggler').click(function() {
+    console.log("testing toggler question repetition")
+    makeNewQuestionAppear(questionCounter)
+    $('#inbetweenQuestions').hide()
+    $('#userInput').show()
+    isPaused = false
+})
 
 
+//the remaining functions deal with pronounciation related events
+$('.pronounciationButton').click(function() {
+    console.log("clicking on the pronounciation button")
+    $('#pronounciationWrapper').show()
+    isPaused = true
+})
 
-        //the following closes a pop-up <div>
-        $('.close').click(function(event) {
-                var $target = $(event.target)
-                if ($target.hasClass('close')) {
-                    $('#pronounciationWrapper').css('display', 'none')
-                    console.log("hide this")
-                }
-            }) //end of close listener
 
-    } //end of addEventListeners()
+$('.uniqueConsonantsToggler').click(function() {
+    $('#uniqueConsonantsTable').show()
+    $('#diphthongsTable').hide()
+    $('#samePronounciationTable').hide()
+    $('#vowelsTable').hide()
+    console.log("unique consonants")
+})
 
-$(document).ready(function() {
-    addEventListeners();
+$('.vowelsToggler').click(function() {
+    $('#vowelsTable').css('display', 'table')
+    $('#diphthongsTable').hide()
+    $('#uniqueConsonantsTable').hide()
+    $('#samePronounciationTable').hide()
+    console.log("vowels should appear")
+})
+
+
+$('.diphthongsToggler').click(function() {
+    $('#diphthongsTable').css('display', 'table')
+    $('#vowelsTable').hide()
+    $('#uniqueConsonantsTable').hide()
+    $('#samePronounciationTable').hide()
+    console.log("diphthongs should appear")
+})
+
+$('.samePronounciationToggler').click(function() {
+    $('#samePronounciationTable').css('display', 'table')
+    $('#diphthongsTable').hide()
+    $('#vowelsTable').hide()
+    $('#uniqueConsonantsTable').hide()
+})
+
+$('.close').click(function() {
+    var $target = $(event.target)
+    if ($target.hasClass('close')) {
+        $('#pronounciationWrapper').css('display', 'none')
+        console.log("hide this")
+    }
 })
